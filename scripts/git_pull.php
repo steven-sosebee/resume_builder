e8055a99257c88b841078d4c7c8a86c9897161b6
Steven Sosebee <swsosebee@gmail.com>
2023-10-29 12:10:25 -0400

    updated pull script for automation

--- scripts/git_pull.php
+++ scripts/git_pull.php
@@ -2,7 +2,11 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 // input the application's home folder
 $folder = "public_html/resume-builder";
+echo 'Running the file...';
 if ( $_POST['payload'] ) {
-   shell_exec("cd ~/$location && git pull origin gh-pages");
+   echo "Executing the shell script...";
+   shell_exec("cd ~/$folder && git pull origin gh-pages");
 }
+
+echo "Finished...";
 ?>
