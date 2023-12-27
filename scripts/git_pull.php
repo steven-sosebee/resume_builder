20239d2adac9248096e024543ade953b8236857d
Steven Sosebee <swsosebee@gmail.com>
2023-12-27 01:53:40 -0500

    updated static information

--- scripts/git_pull.php
+++ scripts/git_pull.php
@@ -2,11 +2,15 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 // input the application's home folder
 $folder = "public_html/resume-builder";
-echo 'Running the file...';
-if ( $_POST['payload'] ) {
-   echo "Executing the shell script...";
-   shell_exec("cd ~/$folder && git pull origin gh-pages");
-}
+date_default_timezone_set('America/New_York');
 
-echo "Finished...";
+// echo "Running the file...\n";
+file_put_contents("log.txt",date('Y-m-d H:i:s')." - Starting update...", FILE_APPEND);
+if ( $_POST['payload'] ) {
+   // echo "Executing the shell script...\n";
+   shell_exec("cd ~/public_html/resume-builder && git pull origin gh-pages");
+   file_put_contents("log.txt","Shell executed...", FILE_APPEND);
+}
+file_put_contents("log.txt","Finished...\n", FILE_APPEND);
+// echo "Finished...";
 ?>
